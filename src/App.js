6e12479290c138099cc73a99ab2041e0b3e65ca9
daniel-raj <dani85rj@gmail.com>
2022-09-06 19:29:25 +0530

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Header from './Components/Header';
 import UrlShortner from './Components/UrlShortner';
+import ReactGa from 'react-ga'
+
+const TRACKING_ID="G-LQ0083ERCY"
+ReactGa.initialize(TRACKING_ID)
 
 function App() {
   return (
