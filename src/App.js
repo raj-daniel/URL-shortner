7e567a4cdca11bfaffde9a0067e193bb88868274
daniel-raj <dani85rj@gmail.com>
2022-09-06 19:44:53 +0530

    configure google analytics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import Header from './Components/Header';
-import UrlShortner from './Components/UrlShortner';
-import ReactGa from 'react-ga'
+import "./App.css";
+import Header from "./Components/Header";
+import UrlShortner from "./Components/UrlShortner";
+import ReactGa from "react-ga";
 
-const TRACKING_ID="G-LQ0083ERCY"
-ReactGa.initialize(TRACKING_ID)
+const TRACKING_ID = "G-LQ0083ERCY";
+ReactGa.initialize(TRACKING_ID);
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <UrlShortner/>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <UrlShortner />
+        </div>
+    );
 }
 
 export default App;
